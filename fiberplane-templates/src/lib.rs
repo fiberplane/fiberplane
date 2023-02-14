6646eecab66c24701700403bd48209cfe9f55890
Arend van Beelen jr <arendjr@gmail.com>
2023-02-14 12:04:09 +0100

    Add documentation fields and generate READMEs

--- fiberplane-templates/src/lib.rs
+++ fiberplane-templates/src/lib.rs
@@ -1,3 +1,75 @@
+/*!
+# Fiberplane Templates
+
+> Programmatically generate Fiberplane Notebooks for repeatable workflows
+
+## Overview
+
+Fiberplane Templates are built with the [Jsonnet](https://jsonnet.org/) data
+templating language.
+
+This crate includes:
+
+- Fiberplane [Jsonnet library](./fiberplane.libsonnet) with functions for
+  creating notebooks
+  ([API Docs](https://docs.fiberplane.com/reference/templates-api))
+- Rust library for expanding templates into notebooks and for converting
+  existing notebooks into templates
+
+## Quickstart
+
+The [Fiberplane CLI](https://github.com/fiberplane/fp) is the recommended way to
+interact with Templates (see the
+[docs](https://github.com/fiberplane/fp#templates) or run `fp help templates`).
+
+## Structure of a Template
+
+Most Fiberplane Templates export a Jsonnet function that accepts some parameters
+and creates a notebook using the helper functions provided by the Fiberplane
+Jsonnet library.
+
+```jsonnet
+local fp = import 'fiberplane.libsonnet';
+local c = fp.cell;
+local fmt = fp.format;
+
+// Parameters are named and can have default values
+function(incidentName='API Outage')
+  fp.notebook
+    .new('Incident Response for: ' + incidentName)
+    .setTimeRangeRelative(minutes=60)
+    .addCells([
+      // The library exposes helper functions for creating every cell type
+      c.h1('Heading'),
+      c.text(
+        // There are also helper functions for formatting text
+        fmt.bold('Hello World!')
+      )
+    ])
+```
+
+See the
+[examples](https://github.com/fiberplane/fiberplane/tree/main/fiberplane-templates/examples)
+for more detailed, use-case-specific templates.
+
+## Snippets
+
+Snippets are smaller pieces of Jsonnet code that produce reusable arrays of
+notebook cells, rather than whole notebooks.
+
+```jsonnet
+local fp = import 'fiberplane.libsonnet';
+local c = fp.cell;
+local fmt = fp.format;
+
+fp.snippet([
+  c.h2('I am a snippet'),
+  c.code('Here is some code'),
+])
+```
+
+*/
+
 #[cfg(feature = "convert")]
 mod convert;
 #[cfg(feature = "examples")]
