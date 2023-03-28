d9f87e5676ee9435e266ca5ef39a75c5cf5d32d6
Gerry Agbobada <gerry@fiberplane.com>
2023-03-28 17:09:12 +0200

    Sort versions from crates-io in semantic order

--- fiberplane-ci/src/utils/crates.rs
+++ fiberplane-ci/src/utils/crates.rs
@@ -66,9 +66,8 @@ pub async fn get_previous_alpha_version(
 ///
 /// Notably, this _includes_ the yanked versions.
 ///
-/// There is no guarantee on the order in the resulting list, it is most
-/// likely to be ordered from older publication date to newest publication date.
-/// Note that "yanking" a version is a publication event, and might change the ordering.
+/// The versions are sorted from smallest semver-wise (0.0.0-alpha.0) to most
+/// recent semver-wise (9999.9999.9999).
 async fn get_published_versions(index_url: &str, crate_name: &str) -> Result<Vec<String>> {
     let index_url = format!("{index_url}/{}", index_url_path(crate_name));
     let client = Client::new();
@@ -79,7 +78,7 @@ async fn get_published_versions(index_url: &str, crate_name: &str) -> Result<Vec
         .await?;
     let response_text = response.text().await?;
 
-    response_text
+    let mut versions = response_text
         .split('\n')
         .map(str::trim)
         .filter(|line| !line.is_empty())
@@ -90,7 +89,11 @@ async fn get_published_versions(index_url: &str, crate_name: &str) -> Result<Vec
                 Err(error.into())
             }
         })
-        .collect()
+        .collect::<Result<Vec<String>>>()?;
+
+    sort_versions(&mut versions);
+
+    Ok(versions)
 }
 
 /// Helper for generating index path to crates
@@ -122,6 +125,95 @@ pub async fn is_published(index_url: &str, crate_name: &str, version: &str) -> R
     Ok(versions.iter().any(|published| published == version))
 }
 
+fn sort_versions(versions: &mut [String]) {
+    versions.sort_by(|lhs, rhs| {
+        let (l_major, l_minor, l_patch, l_meta) =
+            parse_version(lhs).expect("crates-io returns valid version numbers.");
+        let (r_major, r_minor, r_patch, r_meta) =
+            parse_version(rhs).expect("crates-io returns valid version numbers.");
+        if l_major != r_major {
+            return l_major.cmp(&r_major);
+        }
+        if l_minor != r_minor {
+            return l_minor.cmp(&r_minor);
+        }
+        if l_patch != r_patch {
+            return l_patch.cmp(&r_patch);
+        }
+        match (l_meta, r_meta) {
+            (Some(_), None) => std::cmp::Ordering::Less,
+            (None, Some(_)) => std::cmp::Ordering::Greater,
+            (None, None) => std::cmp::Ordering::Equal,
+            (Some(l_meta), Some(r_meta)) => cmp_prereleases(l_meta, r_meta),
+        }
+    });
+}
+
+// NOTE: we do not rely on a crate like semver to do the comparison because we
+// have non-semver versions like '1.0.0-beta.2-alpha.3' (with 2 dashes) that
+// semver crate cannot sort as we want out of the box.
+fn cmp_prereleases(lhs: &str, rhs: &str) -> std::cmp::Ordering {
+    assert!(lhs.split('-').count() <= 2, "Comparing prerelease versions for ordering only works when prereleases have at most 1 '-'; {lhs:?} is invalid");
+    assert!(rhs.split('-').count() <= 2, "Comparing prerelease versions for ordering only works when prereleases have at most 1 '-'; {rhs:?} is invalid");
+
+    /// Computes ordering between components like 'alpha.2' and 'beta.5',
+    /// making sure that 'alpha.14' < 'alpha.3'
+    fn cmp_prerelease_sections(lhs: &str, rhs: &str) -> std::cmp::Ordering {
+        match (lhs.split_once('.'), rhs.split_once('.')) {
+            (None, None) => lhs.cmp(rhs),
+            (None, Some((r_first, _r_count))) => {
+                if lhs == r_first {
+                    std::cmp::Ordering::Less
+                } else {
+                    lhs.cmp(r_first)
+                }
+            }
+            (Some((l_first, _l_count)), None) => {
+                if l_first == rhs {
+                    std::cmp::Ordering::Greater
+                } else {
+                    l_first.cmp(rhs)
+                }
+            }
+            (Some((l_first, l_count)), Some((r_first, r_count))) => {
+                if l_first != r_first {
+                    l_first.cmp(r_first)
+                } else {
+                    l_count
+                        .parse::<u32>()
+                        .unwrap()
+                        .cmp(&r_count.parse::<u32>().unwrap())
+                }
+            }
+        }
+    }
+
+    match (lhs.split_once('-'), rhs.split_once('-')) {
+        (None, None) => lhs.cmp(rhs),
+        (None, Some((r_first, _r_second))) => {
+            if lhs == r_first {
+                std::cmp::Ordering::Greater
+            } else {
+                cmp_prerelease_sections(lhs, r_first)
+            }
+        }
+        (Some((l_first, _l_second)), None) => {
+            if rhs == l_first {
+                std::cmp::Ordering::Less
+            } else {
+                cmp_prerelease_sections(l_first, rhs)
+            }
+        }
+        (Some((l_first, l_second)), Some((r_first, r_second))) => {
+            if l_first != r_first {
+                cmp_prerelease_sections(l_first, r_first)
+            } else {
+                cmp_prerelease_sections(l_second, r_second)
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -132,5 +224,35 @@ mod tests {
         assert_eq!(&index_url_path("fib"), "3/f/fib");
         assert_eq!(&index_url_path("fi"), "2/fi");
         assert_eq!(&index_url_path("f"), "1/f");
+    }
+
+    #[test]
+    fn test_versions_sort() {
+        let mut start = vec![
+            "1.1.0".to_string(),
+            "1.1.1".to_string(),
+            "1.0.1".to_string(),
+            "0.1.0".to_string(),
+            "1.1.0-alpha.12".to_string(),
+            "1.1.0-beta.1".to_string(),
+            "1.1.0-beta.1-alpha.2".to_string(),
+            "1.1.0-alpha.2".to_string(),
+        ];
+
+        sort_versions(&mut start);
+
+        assert_eq!(
+            start,
+            vec![
+                "0.1.0".to_string(),
+                "1.0.1".to_string(),
+                "1.1.0-alpha.12".to_string(),
+                "1.1.0-alpha.2".to_string(),
+                "1.1.0-beta.1-alpha.2".to_string(),
+                "1.1.0-beta.1".to_string(),
+                "1.1.0".to_string(),
+                "1.1.1".to_string(),
+            ]
+        );
     }
 }
