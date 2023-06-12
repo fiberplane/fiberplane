8e232a99689d9cdd100acc334ceded426f87d7dc
Arend van Beelen jr <arendjr@gmail.com>
2023-06-12 14:13:12 +0200

    Remove GitHub utils

--- fiberplane-ci/src/utils/mod.rs
+++ fiberplane-ci/src/utils/mod.rs
@@ -1,6 +1,5 @@
 pub mod crates;
 pub mod git;
-pub mod github;
 pub mod toml_node;
 pub(crate) mod toml_patcher;
 pub mod versions;
@@ -8,7 +7,6 @@ pub mod workspaces;
 
 pub use crates::*;
 pub use git::*;
-pub use github::*;
 pub use toml_node::*;
 pub use versions::*;
 pub use workspaces::*;
