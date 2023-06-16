d9b08c31627bdd58b64bcf20ce17502550642442
Arend van Beelen <arendjr@gmail.com>
2023-06-16 11:45:13 +0200

    Remove utils only used by fpx

--- fiberplane-ci/src/utils/mod.rs
+++ fiberplane-ci/src/utils/mod.rs
@@ -1,5 +1,4 @@
 pub mod crates;
-pub mod git;
 pub mod github;
 pub mod toml_node;
 pub(crate) mod toml_patcher;
@@ -7,7 +6,6 @@ pub mod versions;
 pub mod workspaces;
 
 pub use crates::*;
-pub use git::*;
 pub use github::*;
 pub use toml_node::*;
 pub use versions::*;
