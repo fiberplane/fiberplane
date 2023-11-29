924d97e5e78e78fae5c777f86977eab6a971f51c
oscarvz <oscar@fiberplane.com>
2023-11-29 15:17:17 +0100

    Add `isIconType` type guard

--- fiberplane-ui/src/components/Icon/Icon.tsx
+++ fiberplane-ui/src/components/Icon/Icon.tsx
@@ -2,6 +2,10 @@ import { ICON_MAP } from "./IconMap";
 
 export type IconType = keyof typeof ICON_MAP;
 
+export function isIconType(key: unknown): key is IconType {
+  return typeof key === "string" && key in ICON_MAP;
+}
+
 type IconProps = React.SVGProps<SVGSVGElement> & {
   iconType: IconType;
 };
