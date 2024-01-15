37dbe373f165cc347129a3c9b33114f920f4f3a7
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-01-15 15:42:30 +0100

    add initial documentation site

---
+++ docs/astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+      ],
+    }),
+  ],
+});
