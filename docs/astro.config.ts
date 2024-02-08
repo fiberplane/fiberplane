fba2d11bfe289b789fa55c80f1618dc83c18fad2
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-02-08 15:39:49 +0100

    make astro config ts

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import vue from "@astrojs/vue";
 
-// https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 1111,
+  },
   integrations: [
+    vue(),
     starlight({
       title: "Fiberplane",
       social: {
         github: "https://github.com/withastro/starlight",
       },
+      components: {
+        Header: "@components/Header.astro",
+      },
       sidebar: [
         {
           label: "Guides",
-          autogenerate: { directory: "guides" },
+          autogenerate: { directory: "docs/guides" },
         },
       ],
     }),
