08321de40b1dbb4ddf1871b1d28a377cea2b58de
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-02-19 09:26:24 +0100

    add prettier because Biome doesn't support Astro yet

---
+++ docs/.prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+  useTabs: false,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
