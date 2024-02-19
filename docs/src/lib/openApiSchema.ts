1570c5d51ecccdbb424132112378e6e996b134aa
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-02-19 09:34:08 +0100

    update formatting for astro and ts files according to fp style

--- docs/src/lib/openApiSchema.ts
+++ docs/src/lib/openApiSchema.ts
@@ -4,19 +4,19 @@ import fs from "fs/promises";
 let schema: SwaggerSpec;
 
 export async function getSchema() {
-	if (schema) {
-		return schema;
-	}
+  if (schema) {
+    return schema;
+  }
 
-	try {
-		const schemaPath = "../schemas/openapi_v1.yml"; // yeah...
+  try {
+    const schemaPath = "../schemas/openapi_v1.yml"; // yeah...
 
-		const schemaFile = await fs.readFile(schemaPath, "utf-8");
-		schema = await parse(schemaFile);
-		// TODO: maybe filter some items
-		return schema;
-	} catch (e) {
-		console.error(e);
-		return;
-	}
+    const schemaFile = await fs.readFile(schemaPath, "utf-8");
+    schema = await parse(schemaFile);
+    // TODO: maybe filter some items
+    return schema;
+  } catch (e) {
+    console.error(e);
+    return;
+  }
 }
