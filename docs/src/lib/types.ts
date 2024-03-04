7ac2cbe1c55cb8f150743d681ce82606e4af1029
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-03-04 14:41:45 +0100

    simplify types

--- docs/src/lib/types.ts
+++ docs/src/lib/types.ts
@@ -1,22 +1,18 @@
-import type { OpenAPI, OpenAPIV2, OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
+import type { OpenAPI, OpenAPIV2, OpenAPIV3 } from "openapi-types";
 
 export type Operation = Omit<OpenAPIV3.OperationObject, "tags"> & {
-  httpVerb: OpenAPIV3.HttpMethods | OpenAPIV3_1.HttpMethods;
+  httpVerb: HttpMethods;
   path: string;
 };
 
-export type TagObject = OpenAPIV3.TagObject | OpenAPIV3_1.TagObject;
+export type TagObject = OpenAPIV3.TagObject
 
 export type ExtendedTagObject = TagObject & {
   operations: Operation[] | undefined;
 };
 
-export type PathItemObject =
-  | OpenAPIV3.PathItemObject
-  | OpenAPIV3_1.PathItemObject;
+export type PathItemObject = OpenAPIV3.PathItemObject;
 
-export type OperationObject =
-  | OpenAPIV3.OperationObject
-  | OpenAPIV3_1.OperationObject;
+export type OperationObject = OpenAPIV3.OperationObject;
 
-export type HttpMethods = OpenAPIV3.HttpMethods | OpenAPIV3_1.HttpMethods;
+export type HttpMethods = OpenAPIV3.HttpMethods;
