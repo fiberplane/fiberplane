bc192cea371044449f0f4a10e66e26096e26a78d
oscarvz <oscar@fiberplane.com>
2024-03-08 15:56:26 +0100

    Fix linting issues in `/docs`

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import markdownIntegration from "@astropub/md";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
