5c7618dc5589034b8f5ecf6276e001fe5d468af5
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2024-03-27 14:13:34 +0100

    fix formatting

--- scripts/validate_openapi.ts
+++ scripts/validate_openapi.ts
@@ -1,5 +1,5 @@
 // This simple script used to validate the OpenAPI schema uses
-// the same @readme/openapi-parser package that our docs builder 
+// the same @readme/openapi-parser package that our docs builder
 // relies on.
 
 import OpenAPIParser from "npm:@readme/openapi-parser";
@@ -8,8 +8,12 @@ import type { OpenAPI } from "npm:openapi-types";
 const schemaPath = "schemas/openapi_v1.yml";
 
 try {
-  const api = await OpenAPIParser.validate(schemaPath) as OpenAPI.Document;
-  console.log("Successfully validated OpenAPI schema: ", api.info.title, api.info.version);
+  const api = (await OpenAPIParser.validate(schemaPath)) as OpenAPI.Document;
+  console.log(
+    "Successfully validated OpenAPI schema: ",
+    api.info.title,
+    api.info.version,
+  );
 } catch (e) {
   console.error(e);
 }
