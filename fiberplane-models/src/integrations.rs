7b89670a29b5526524b7f9f3fc682b5a80af8af8
Brett Beutell <brbeut@gmail.com>
2024-05-08 11:53:57 +0200

    Add support for zoom app uninstallation error

--- fiberplane-models/src/integrations.rs
+++ fiberplane-models/src/integrations.rs
@@ -622,3 +622,61 @@ impl axum_07::response::IntoResponse for ZoomAppDetailsError {
         (status_code, body).into_response()
     }
 }
+
+
+#[derive(Debug, Deserialize, Serialize, Clone, Copy, PartialEq, Eq, Error)]
+#[cfg_attr(
+    feature = "fp-bindgen",
+    derive(Serializable),
+    fp(rust_module = "fiberplane_models::integrations")
+)]
+#[non_exhaustive]
+#[serde(tag = "error", content = "details", rename_all = "snake_case")]
+pub enum ZoomAppUninstallError {
+    #[error("the integration is not installed")]
+    NotInstalled,
+
+    #[error("unknown error occurred")]
+    InternalServerError,
+
+    /// Common auth errors.
+    #[error(transparent)]
+    Auth(AuthError),
+}
+
+impl ZoomAppUninstallError {
+    #[cfg(any(feature = "axum_06", feature = "axum_07"))]
+    fn status_code(&self) -> StatusCode {
+        match self {
+            ZoomAppUninstallError::NotInstalled => StatusCode::NOT_FOUND,
+            ZoomAppUninstallError::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+            ZoomAppUninstallError::Auth(err) => err.status_code(),
+        }
+    }
+}
+
+impl From<AuthError> for ZoomAppUninstallError {
+    fn from(value: AuthError) -> Self {
+        ZoomAppUninstallError::Auth(value)
+    }
+}
+
+#[cfg(feature = "axum_06")]
+impl axum_06::response::IntoResponse for ZoomAppUninstallError {
+    fn into_response(self) -> axum_06::response::Response {
+        let body = serde_json::to_string(&self).expect("unable to serialize error body");
+        let status_code = self.status_code();
+
+        (status_code, body).into_response()
+    }
+}
+
+#[cfg(feature = "axum_07")]
+impl axum_07::response::IntoResponse for ZoomAppUninstallError {
+    fn into_response(self) -> axum_07::response::Response {
+        let body = serde_json::to_string(&self).expect("unable to serialize error body");
+        let status_code = self.status_code();
+
+        (status_code, body).into_response()
+    }
+}
