3afb6a4ae92981d0fdd97667cbccc3a5fb23e604
Stephan Lagerwaard <steph@fiberplane.com>
2024-05-10 12:06:58 +0200

    Remove `react-use` from rollup config

--- fiberplane-hooks/rollup.config.ts
+++ fiberplane-hooks/rollup.config.ts
@@ -11,7 +11,7 @@ export default defineConfig([
       sourcemap: true,
       compact: true,
     },
-    external: ["react", "eventemitter3", "react-use"],
+    external: ["react", "eventemitter3"],
     plugins: [swc(defineRollupSwcOption({ sourceMaps: true }))],
   },
   {
