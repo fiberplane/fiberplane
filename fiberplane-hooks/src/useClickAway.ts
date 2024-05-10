869664cc9755da1808969db2e032c64049f5ac0f
Stephan Lagerwaard <steph@fiberplane.com>
2024-05-10 13:10:09 +0200

    Add documentation to the new hooks

--- fiberplane-hooks/src/useClickAway.ts
+++ fiberplane-hooks/src/useClickAway.ts
@@ -2,6 +2,12 @@ import { type RefObject, useEffect, useRef } from "react";
 import { useDocumentEventHandler } from "./useDocumentEventHandler";
 import { useHandler } from "./useHandler";
 
+/**
+ * Hook that triggers a callback when a click, touch, or specified keyboard events occur outside of the specified element.
+ *
+ * @param {RefObject<HTMLElement | null>} ref - A ref object pointing to the element to monitor for outside interactions.
+ * @param {(event: MouseEvent | TouchEvent | KeyboardEvent) => void} onClickAway - Callback function that gets executed when an outside click, touch, or key event is detected.
+ */
 export const useClickAway = (
   ref: RefObject<HTMLElement | null>,
   onClickAway: (event: MouseEvent | TouchEvent | KeyboardEvent) => void,
--- fiberplane-hooks/src/useDocumentEventHandler.ts
+++ fiberplane-hooks/src/useDocumentEventHandler.ts
@@ -1,5 +1,26 @@
 import { useEffect } from "react";
 
+/**
+ * The `useDocumentEventHandler` hook allows for handling of document-wide events in a React component.
+ * It automatically attaches and cleans up event listeners using the `useEffect` hook.
+ *
+ * @template TEventType extends keyof GlobalEventHandlersEventMap
+ * Type parameter representing the type of the event, constrained to valid types defined in `GlobalEventHandlersEventMap`.
+ *
+ * @param {TEventType | Array<TEventType>} eventType
+ * A single event type or an array of event types for which the event handler will be registered.
+ *
+ * @param {(event: GlobalEventHandlersEventMap[TEventType]) => void} handler
+ * The function that will be called when the event occurs. The event type corresponds to the provided `eventType`.
+ *
+ * @example
+ * // Single event usage
+ * useDocumentEventHandler('click', (event) => console.log('Clicked!', event));
+ *
+ * @example
+ * // Multiple events usage
+ * useDocumentEventHandler(['mousedown', 'mouseup'], (event) => console.log(`Event: ${event.type}`, event));
+ */
 export function useDocumentEventHandler<
   TEventType extends keyof GlobalEventHandlersEventMap,
 >(
