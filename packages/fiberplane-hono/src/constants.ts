47d19651fa7986b9454f3aeff41431668721a14d
Brett Beutell <brbeut@gmail.com>
2025-02-24 18:50:35 +0100

    Tweak constant descriptions

--- packages/fiberplane-hono/src/constants.ts
+++ packages/fiberplane-hono/src/constants.ts
@@ -2,23 +2,23 @@ export const PLAYGROUND_SERVICES_URL =
   "https://playground-services.mies.workers.dev";
 
 /**
- * The environment variable for the Fiberplane OTel token.
+ * Fiberplane otel-worker auth token.
  */
 export const ENV_FIBERPLANE_OTEL_TOKEN = "FIBERPLANE_OTEL_TOKEN";
 
 /**
- * The (deprecated) environment variable for the Fiberplane auth token.
+ * Fiberplane otel-worker auth token.
  * @deprecated Use {@link ENV_FIBERPLANE_OTEL_TOKEN} instead.
  */
 export const ENV_FPX_AUTH_TOKEN = "FPX_AUTH_TOKEN";
 
 /**
- * The environment variable for the Fiberplane OTel endpoint.
+ * Fiberplane otel-worker endpoint (or any other OpenTelemetry collector endpoint)
  */
 export const ENV_FIBERPLANE_OTEL_ENDPOINT = "FIBERPLANE_OTEL_ENDPOINT";
 
 /**
- * The (deprecated) environment variable for the Fiberplane endpoint.
+ * Fiberplane otel-worker endpoint (or any other OpenTelemetry collector endpoint)
  * @deprecated Use {@link ENV_FIBERPLANE_OTEL_ENDPOINT} instead.
  */
 export const ENV_FPX_ENDPOINT = "FPX_ENDPOINT";
