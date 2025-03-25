1065b1106a4098097f887951c1a1bd1224fe98de
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2025-03-26 00:22:45 +0100

    cleanup

--- packages/agents-playground/src/components/Spinner.tsx
+++ packages/agents-playground/src/components/Spinner.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { useStickyLoading } from "@/useStickyLoading";
 import { RefreshCw } from "lucide-react";
 
 const animationDuration = "[animation-duration:1000ms]";
