8e138f519538b773e96ef217f66b88085cbb6633
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2025-03-27 11:32:56 +0100

    make sure to hit the correct url for agents list

--- packages/agents-ui/src/hooks/useListAgents.ts
+++ packages/agents-ui/src/hooks/useListAgents.ts
@@ -4,6 +4,6 @@ import { useQuery } from "@tanstack/react-query";
 export function useListAgents() {
   return useQuery<ListAgentsResponse>({
     queryKey: ["list_agents"],
-    queryFn: () => fetch("/fp-agents/api/agents").then((res) => res.json()),
+    queryFn: () => fetch("/fp/api/agents").then((res) => res.json()),
   });
 }
